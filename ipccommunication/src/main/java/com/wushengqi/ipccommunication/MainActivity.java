58ae428b9f5f823818888a2d75403dd20363cb18
WUSHENGQI\sqwu <441578157@qq.com>
2019-03-12 23:30:36 +0800

    添加进程间通信demo

--- ipccommunication/src/main/java/com/wushengqi/ipccommunication/MainActivity.java
+++ ipccommunication/src/main/java/com/wushengqi/ipccommunication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wushengqi.ipccommunication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.wushengqi.ipccommunication.binder_and_service.BindServiceActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.skipBindService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BindServiceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
