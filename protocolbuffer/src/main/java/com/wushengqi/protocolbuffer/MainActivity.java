851892a1a43102ccb38dc4fac56bbb0a7d9ef1c4
WUSHENGQI\sqwu <441578157@qq.com>
2019-06-16 16:38:49 +0800

    future

--- protocolbuffer/src/main/java/com/wushengqi/protocolbuffer/MainActivity.java
+++ protocolbuffer/src/main/java/com/wushengqi/protocolbuffer/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "ddd", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
+        
     }
 }
