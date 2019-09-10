d01ed97cfb68a91461d9e05ec6811e62fda2315a
WUSHENGQI\sqwu <441578157@qq.com>
2019-09-10 23:27:37 +0800

    自定义录制视频

--- sharedscreen/src/main/java/com/example/sharedscreen/encoder2/MediaCodecCreater.java
+++ sharedscreen/src/main/java/com/example/sharedscreen/encoder2/MediaCodecCreater.java
@@ -2,17 +2,26 @@ package com.example.sharedscreen.encoder2;
 
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
 import android.media.MediaFormat;
 import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
+import android.view.Surface;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashSet;
 
+/**
+ * 参考 1.https://www.jianshu.com/p/9512defaa8c1
+ */
 public class MediaCodecCreater extends HandlerThread {
     private MediaFormat mediaFormat;
+    private Surface mSurface;
+
     private int fps = 60;
     //子线程的handler
     private Handler mediaCodecHandler = new Handler(this.getLooper(), new MediaCodecCallback());
@@ -72,11 +81,14 @@ public class MediaCodecCreater extends HandlerThread {
 
     }
 
+    private MediaCodec mMediaCodec;
     private void initMediaCodec(){
         try {
             initFormat();
-            MediaCodec mediaCodec = MediaCodec.createByCodecName(MediaFormat.MIMETYPE_VIDEO_AVC);
-            mediaCodec.configure(mediaFormat, mediaCodec.createInputSurface(), null, 0);
+            mMediaCodec = MediaCodec.createByCodecName(MediaFormat.MIMETYPE_VIDEO_AVC);
+            //创建一个surface，作为全局变量的原因是因为VirtualPlay要用
+            mSurface = mMediaCodec.createInputSurface();
+            mMediaCodec.configure(mediaFormat, mSurface, null, 0);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -92,4 +104,46 @@ public class MediaCodecCreater extends HandlerThread {
         }
     }
 
+    //开始录制
+    public void startRecoder(){
+        mMediaCodec.start();
+        ByteBuffer[] inputByteBuffer = mMediaCodec.getInputBuffers();
+        ByteBuffer[] outputByteBuffer = mMediaCodec.getOutputBuffers();
+
+
+    }
+
+    /**
+     * 根据MIME_TYPE = "video/avc"选择设备支持的编码器和colorFormat
+     * @param mimeType
+     * @return
+     */
+    private MediaCodecInfo selectSupportCodec(String mimeType) {
+        int numCodecs = MediaCodecList.getCodecCount();
+        for (int i = 0; i < numCodecs; i++) {
+            MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
+            if (!codecInfo.isEncoder()) {
+                continue;
+            }
+            String[] types = codecInfo.getSupportedTypes();
+            for (int j = 0; j < types.length; j++) {
+                if (types[j].equalsIgnoreCase(mimeType)) {
+                    return codecInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据mime类型匹配编码器支持的颜色格式
+     */
+    private int selectSupportColorFormat(MediaCodecInfo mCodecInfo, String mimeType) {
+        MediaCodecInfo.CodecCapabilities capabilities = mCodecInfo.getCapabilitiesForType(mimeType);
+        HashSet<Integer> colorFormats = new HashSet<>();
+        for(int i : capabilities.colorFormats) colorFormats.add(i);
+        if(colorFormats.contains(MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar)) return MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar;
+        if(colorFormats.contains(MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar)) return MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar;
+        return 0;
+    }
 }
