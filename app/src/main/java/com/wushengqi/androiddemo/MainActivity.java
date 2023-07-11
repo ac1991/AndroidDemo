6c8a32092f54380a3581eaf38fb7f875f7ed5693
wushengqi <441578157@qq.com>
2023-07-11 16:36:39 +0800

    主分支提交

--- app/src/main/java/com/wushengqi/androiddemo/MainActivity.java
+++ app/src/main/java/com/wushengqi/androiddemo/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("主分支提交", "");
         fix();
         initView();
 
